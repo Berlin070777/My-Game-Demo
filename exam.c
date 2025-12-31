c1c8dcccec598ab8896baddfe73b4a26a08a0b74
Pauline Sue <3299034597@qq.com>
2025-12-31 17:58:34 +0800

    Create exam.c

---
+++ exam.c
@@ -0,0 +1,21 @@
+//这是根据我之前所学的，一道练手的题目，背景： 你是一名班主任，你的班级刚开始建立
+//人数不确定，你需要一个系统来录入学生，并且能随时查看全班的成绩情况。班级人数会随着录入不断增加。
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[40];
+    int age;
+    char gender;
+};
+
+struct ReportCard {
+    char name[40];
+    float score;
+};
+
+int main() {
+    
+}
+
